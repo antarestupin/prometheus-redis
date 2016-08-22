b7c226559fc91ca347edb024b5ddd557ed5e08c1
Antares Tupin <antares.tupin@blablacar.com>
2016-08-22 17:45:27 +0200

    Added support for multiple redis connections

--- exporter.py
+++ exporter.py
@@ -6,18 +6,19 @@ from prometheus_client import start_http_server
 from prometheus_client.core import GaugeMetricFamily, REGISTRY
 
 class RedisExporter(object):
-    def __init__(self, query, r):
+    def __init__(self, query, redisConnections):
         self.query = query
-        self.redis = r
+        self.redisConnections = redisConnections
 
     def collect(self):
         for metric_name in self.query['metrics']:
             metric = self.query['metrics'][metric_name]
+            value = self.redisConnections[metric['connection']].execute_command(metric['query'])
 
             yield GaugeMetricFamily(
                 metric.get('name', metric_name),
                 metric['description'],
-                value = float(self.redis.execute_command(metric['query']))
+                value = float(value)
             )
 
 if __name__ == "__main__":
@@ -30,14 +31,16 @@ if __name__ == "__main__":
         except yaml.YAMLError as e:
             print(e)
 
-    conn = query['connection']
-    r = redis.Redis(
-        host = conn.get('host', 'localhost'),
-        port = conn.get('port', 6379),
-        db = conn.get('database', 0),
-        password = conn.get('password', None)
-    )
+    redisConnections = {}
+    for conn in query['connections']:
+        connParams = query['connections'][conn]
+        redisConnections[conn] = redis.Redis(
+            host = connParams.get('host', 'localhost'),
+            port = connParams.get('port', 6379),
+            db = connParams.get('database', 0),
+            password = connParams.get('password', None)
+        )
 
-    REGISTRY.register(RedisExporter(query, r))
+    REGISTRY.register(RedisExporter(query, redisConnections))
     start_http_server(9118)
     while True: time.sleep(1)
